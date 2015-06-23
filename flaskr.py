c3eebe2ba5c892bbc6ced0f5819fe741da386773
Ben Novack <ben@manywords.net>
2015-06-22 20:14:56 -0400

    finished step 3

--- flaskr.py
+++ flaskr.py
@@ -1,7 +1,7 @@
-# http://flask.pocoo.org/docs/0.10/tutorial/setup/#tutorial-setup
-
+#http://flask.pocoo.org/docs/0.10/tutorial/dbcon/#tutorial-dbcon
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
 
 #config
 DATABASE = '/tmp/flaskr.db'
@@ -13,12 +13,24 @@ PASSWORD = 'default'
 #create the app!
 app = Flask(__name__)
 app.config.from_object(__name__)
-#later, change to:
+#TODO, change to:
 #app.config.from_envar('FLASKR_SETTINGS', silent=True)
 
+
+#Database Setup
 
 def connect_db():
         return sqlite3.connect(app.config['DATABASE'])
 
+def init_db():
+        with closing(connect_db()) as db:
+            with app.open_resource('schema.sql', mode='r') as f:
+                db.cursor().executescript(f.read())
+            db.commit()
+
+
+
 if __name__ == '__main__':
         app.run()
+#TODO: Check out http://flask.pocoo.org/docs/0.10/quickstart/#public-server
+#Make it publically available
